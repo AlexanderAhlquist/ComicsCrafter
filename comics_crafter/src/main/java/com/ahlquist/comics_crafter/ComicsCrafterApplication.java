e6b73a99e3a601eb08ac24de8c66845f5fca16ad
Alexander Ahlquist <ahlquist.alexander@gmail.com>
2022-10-12 15:32:02 -0400

    first commit

---
+++ comics_crafter/src/main/java/com/ahlquist/comics_crafter/ComicsCrafterApplication.java
@@ -0,0 +1,13 @@
+package com.ahlquist.comics_crafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicsCrafterApplication.class, args);
+	}
+
+}
---
+++ comics_crafter/src/test/java/com/ahlquist/comics_crafter/ComicsCrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahlquist.comics_crafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsCrafterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
